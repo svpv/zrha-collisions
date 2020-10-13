bc7c317bee1f60d11b7837c6be2f466d59a73cd9
Alexey Tourbin <alexey.tourbin@gmail.com>
2020-10-13 05:31:16 +0300

    zeroes8.c: the state deteriorates due to non-invertible mixing
    The ZrHa construction uses non-invertible mixing, which is generally
    considered a bad thing.  To see how bad it can get in practice, I
    downscale the construction by a factor of 4 (so that it uses 8x8->16
    multiplication).
    
    The hashing process occurs in two phases: feed and mix.  After a portion
    of input data is fed into the internal state (usually with XOR or ADD),
    the state needs to be "mixed": we need to disrupt the state and amplify
    the effect of each input bit, so that the next portion of input data
    cannot cancel out with the previous portion.  Mixing is said to be
    invertible if it can be undone.  For example, the following mixing
    step, with the state variables x and y, is invertible:
    
        m = x * C;
        y += m;
    
    It can be undone:
    
        m = x * C;
        y -= m;
    
    But the following mixing step, resembling the ZrHa construction, cannot
    be undone:
    
        m0 = x * C;
        m1 = y * C;
        y += m0;
        x += m1;
    
    It "ties the knot", it's no longer possible to figure out what needs
    to be subtracted from x and y.  To keep it invertible, we would need
    to execute the two steps successively:
    
        m0 = x * C;
        y += m0;
        m1 = y * C;
        x += m1;
    
    But this results in a less efficient sequence of instructions, because
    multiplications m0 and m1 cannot execute in parallel.  Invertible mixing
    can execute more efficiently only if we have at least 3 state variables.
    
    The fact that the mixing step cannot be undone actually means that the
    mapping from the original state to the mixed state is not 1-to-1 (not a
    permutation).  All inputs are possible, but some outputs are not
    possible, so a few inputs must map to the same output.  Roughly speaking,
    it loses data.  This is why non-invertible mixing is usually frowned
    upon as contributing to collisions.
    
    However, it may or may not be all that bad in practice.  The best case
    is, after the state is mixed and cannot assume certain values, so what,
    after feeding the next portion of data we may reasonably claim that the
    state can assume any possible value again, more or less uniformly.  The
    worst case though is that the state can deteriorate progressively,
    assuming fewer and fewer possible values after each mixing steps.
    This happens when you feed in zeroes.  Eventually the state "collapses":
    one of the multipliers becomes zero, and the state alternates between
    two (or sometimes four) possible values, as in this example:
    
        3500 2d00 bd00 bb00
        002d 0035 00bb 00bd
        3500 2d00 bd00 bb00
        002d 0035 00bb 00bd
    
    But if we look at it from another angle, non-invertible mixing has a
    plus side.  For one, you cannot recover the seed (other than by brute
    force), and so you shouldn't be able to generate collisions too easily.
    Bob Jenkins worried about the mixing functions that can "spontaneously
    unmix".  Well, "tying the knot" will never unmix.  So it all depends
    on how fast the state can deteriorate in practice.
    
    The key realization I had a few days ago is that the rate of
    deterioration actually does not depend (exclusively) on how wide
    multiplication is.  Rather it depends on how big the whole state is.
    In other words, if we update SIMD lanes/registers independently, we
    expose ourselves to much higher deterioration than necessary.  If
    instead we intermix the state among the two SIMD registers (which comes
    for free in practice), the number of zero-updates before the state
    collapses improves from 2^k to at least 2^(2k).
    
    $ gcc -O2 zeroes8.c && ./a.out A |head -1024 |cut -f2 |stats.pl
    q25=587
    q50=1482
    q75=3117
    $ gcc -O2 zeroes8.c && ./a.out B |head -256 |cut -f2 |stats.pl
    q25=20790430
    q50=52553590
    q75=98318214
    $ gcc -O2 zeroes8.c && ./a.out C |head -256 |cut -f2 |stats.pl
    q25=61713577
    q50=181047618
    q75=400268192

---
+++ zeroes8.c
@@ -0,0 +1,154 @@
+// Copyright (c) 2019, 2020 Alexey Tourbin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/auxv.h>
+
+static __uint128_t rand64state;
+
+static __attribute__((constructor)) void rand64init(void)
+{
+    memcpy(&rand64state, (void *) getauxval(AT_RANDOM), 16);
+    rand64state |= 1;
+}
+
+static inline uint64_t rand64(void)
+{
+    uint64_t ret = rand64state >> 64;
+    rand64state *= 0xda942042e4dd58b5;
+    return ret;
+}
+
+static inline uint32_t rotl16(unsigned x, int k) { return x << k | x >> (16 - k); }
+static inline uint32_t rotr16(unsigned x, int k) { return x >> k | x << (16 - k); }
+
+// The original/naive ZrHa construction with two scaled-down SIMD registers.
+// The bits in each register are mixed independently.  On average, the state
+// collapses after only about 2^11 updates.
+static inline void updateA(uint16_t x[2], uint16_t y[2])
+{
+    uint16_t mx[2], my[2];
+    mx[0] = (uint8_t) x[0] * (x[0] >> 8);
+    mx[1] = (uint8_t) x[1] * (x[1] >> 8);
+    my[0] = (uint8_t) y[0] * (y[0] >> 8);
+    my[1] = (uint8_t) y[1] * (y[1] >> 8);
+    mx[0] += rotl16(x[1], 8);
+    mx[1] += rotl16(x[0], 8);
+    my[0] += rotl16(y[1], 8);
+    my[1] += rotl16(y[0], 8);
+    x[0] = mx[0];
+    x[1] = mx[1];
+    y[0] = my[0];
+    y[1] = my[1];
+}
+
+// An improved construction: we multiply out x with y, so the state gets
+// intermixed between the two registers.  The state collapses after about
+// 2^25 updates.
+static inline void updateB(uint16_t x[2], uint16_t y[2])
+{
+    uint16_t mx[2], my[2];
+    mx[0] = (uint8_t) x[0] * (y[0] >> 8);
+    mx[1] = (uint8_t) x[1] * (y[1] >> 8);
+    my[0] = (uint8_t) y[0] * (x[0] >> 8);
+    my[1] = (uint8_t) y[1] * (x[1] >> 8);
+    mx[0] += rotl16(x[1], 8);
+    mx[1] += rotl16(x[0], 8);
+    my[0] += rotl16(y[1], 8);
+    my[1] += rotl16(y[0], 8);
+    x[0] = mx[0];
+    x[1] = mx[1];
+    y[0] = my[0];
+    y[1] = my[1];
+}
+
+// Multiply out x with y in a different order, to elongate the datapath cycle.
+// The state collapses after about 2^27 updates.
+static inline void updateC(uint16_t x[2], uint16_t y[2])
+{
+    uint16_t mx[2], my[2];
+    mx[0] = (uint8_t) x[0] * (y[0] >> 8);
+    mx[1] = (uint8_t) x[1] * (y[1] >> 8);
+    my[0] = (uint8_t) y[0] * (x[1] >> 8);
+    my[1] = (uint8_t) y[1] * (x[0] >> 8);
+    mx[0] += rotl16(x[1], 8);
+    mx[1] += rotl16(x[0], 8);
+    my[0] += rotl16(y[1], 8);
+    my[1] += rotl16(y[0], 8);
+    x[0] = mx[0];
+    x[1] = mx[1];
+    y[0] = my[0];
+    y[1] = my[1];
+}
+
+static inline unsigned try(uint64_t seed, uint32_t imax,
+	void (*update)(uint16_t x[2], uint16_t y[2]))
+{
+    uint16_t x[4];
+    memcpy(x, &seed, 8);
+    uint32_t i = 0;
+    while (1) {
+	int same;
+	uint16_t a[4];
+	memcpy(a, x, 8);
+	update(x, x + 2);
+	update(x, x + 2);
+	update(x, x + 2);
+	update(x, x + 2);
+	same = (x[0] == a[0]) + (x[1] == a[1]) + (x[2] == a[2]) + (x[3] == a[3]);
+	if (same >= 2 || i >= imax)
+	    return i;
+	i += 4;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int which;
+    if (argc == 1)
+	which = 1;
+    else {
+	assert(argc == 2);
+	char c = *argv[1];
+	if (c == 'A')
+	    which = 0;
+	else if (c == 'B')
+	    which = 1;
+	else {
+	    assert(c == 'C');
+	    which = 2;
+	}
+    }
+    while (1) {
+	uint64_t seed = rand64();
+	unsigned n;
+	if (which == 0)
+	    n = try(seed, 1<<13, updateA);
+	else if (which == 1)
+	    n = try(seed, 1<<28, updateB);
+	else
+	    n = try(seed, 1<<29, updateC);
+	printf("%016lx\t%u\n", seed, n);
+    }
+}
